89e8d5f636bed909656c9ddfd7c3fd00d700054a
Rodolfo Cabral Neves <roddas360@gmail.com>
2024-10-23 01:03:38 -0300

    clean the memory

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(int argc, char **argv){
 	
 	directory dir = directory_list();
 	display_pids(dir);
-	//free_directory_list_memory(dir);
+	free_directory_list_memory(dir);
 	return EXIT_SUCCESS;
 }
